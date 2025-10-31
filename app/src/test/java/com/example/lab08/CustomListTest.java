2221766e8efc51fd6631745b5479dca6e41508cf
svskaushik <shaurya9702@gmail.com>
2025-10-31 05:09:32 -0600

    implement test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        // This will fail to compile initially because hasCity() isn't implemented yet
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDoesNotHaveCity() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+        // This will also fail initially for the same reason
+        assertFalse(list.hasCity(edmonton));
+    }
+
 }
